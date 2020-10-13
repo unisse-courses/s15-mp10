f19ca0842cc27114dc63e2c80d7b7301d9ccc9b2
Qijuso <61452378+Qijuso@users.noreply.github.com>
2020-10-13 09:37:56 +0800

    .

--- models/New folder/comments.js
+++ models/New folder/comments.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccapdev.eadlr.mongodb.net/<dbname>?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => { console.log('comments'); },
+        err => {
+            console.log('theres problems');
+        });
+
+var db = mongoose.connection;
+
 
 const commentSchema = new mongoose.Schema({
-    _id: String,
-    author: String,
-    content: String
-});
+    commentID: {type: Number, required: true},
+    author: {type: String, required: true},
+    content: {type: String, required: true}
+}, { collection: "Comments" });
 
-module.exports = mongoose.model('comments', commentSchema);
+commentSchema.methods.recordComment = async function() {
+    var result = commentsModel.create(this);
+    console.log(JSON.stringify(result));
+    return result;
+};
+
+const commentsModel = db.model('Comments', commentSchema);
+
+module.exports = commentsModel;
--- models/New folder/images.js
+++ models/New folder/images.js
@@ -8,3 +8,4 @@ const imageSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('images', imageSchema);
+
