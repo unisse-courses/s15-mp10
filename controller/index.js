35b29f76a20c0eaa84d4d2da4320ca9f59b7751a
Qijuso <61452378+Qijuso@users.noreply.github.com>
2020-10-15 05:16:07 +0800

    .

--- controller/index.js
+++ controller/index.js
@@ -3,9 +3,17 @@ const handlebars = require('handlebars');
 
 const indexFunctions = {
     getHomepage: function (req, res) {
-        res.render('homepage', {
-            title: 'ReviewMe'
-        });
+        if (req.session.type) { // if req.session.type == true
+            res.render('homepage', {
+                title: 'ReviewMe',
+                user: req.session.userName
+            });
+        } else { // if req.session.type == false
+            res.render('homepage', {
+                title: 'ReviewMe',
+                user: 'guest'
+            });
+        }
     },
 }
 
