3bac1f74984fd4ce403ed8d076df55e69e9a62fd
Qijuso <61452378+Qijuso@users.noreply.github.com>
2020-10-14 13:23:56 +0800

    .

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const handlebars = require('handlebars');
 
 const indexFunctions = {
-    
+    getHomepage: function (req, res) {
+        res.render('homepage', {
+            title: 'ReviewMe'
+        });
+    },
 }
 
 module.exports = indexFunctions;
--- router/indexRouter.js
+++ router/indexRouter.js
@@ -3,6 +3,7 @@ const router = express();
 const controller = require('../controller/index');
 const indexMiddleware = require('../middlewares/indexMiddleware');
 // GETS
+router.get('/', controller.getHomepage);
 // ACTIONS
 // POSTS
 module.exports = router;
