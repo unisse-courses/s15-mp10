8cbe5cb0999457d5fd3d0a92d70de2789cdbc969
Qijuso <61452378+Qijuso@users.noreply.github.com>
2020-10-15 13:14:34 +0800

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -4,11 +4,13 @@ const handlebars = require('handlebars');
 const indexFunctions = {
     getHomepage: function (req, res) {
         if (req.session.type) { // if req.session.type == true
+            console.log(req.session.type);
             res.render('homepage', {
                 title: 'ReviewMe',
                 user: req.session.userName
             });
         } else { // if req.session.type == false
+            console.log(req.session.type);
             res.render('homepage', {
                 title: 'ReviewMe',
                 user: 'guest'
