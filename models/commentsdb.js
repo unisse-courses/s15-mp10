dd3b1f184b23e1d415c58be7372ae64ff20dd20a
Qijuso <61452378+Qijuso@users.noreply.github.com>
2020-12-03 17:17:43 +0800

    clean model format

--- models/commentsdb.js
+++ models/commentsdb.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccapdev.eadlr.mongodb.net/ReviewMe?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => { console.log('commentsdb: OK'); },
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccapdev.eadlr.mongodb.net/ReviewMe?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+            console.log('commentsdb: OK');
+        },
         err => {
             console.log('commentsdb: ');
             console.log(err);
@@ -9,12 +14,23 @@ var db = mongoose.connection;
 
 
 const commentSchema = new mongoose.Schema({
-    commentID: {type: Number, required: true},
-    author: {type: String, required: true},
-    content: {type: String, required: true}
-}, { collection: "Comments" });
+    commentID: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    collection: "Comments"
+});
 
-commentSchema.methods.recordComment = async function() {
+commentSchema.methods.recordComment = async function () {
     var result = commentsModel.create(this);
     console.log(JSON.stringify(result));
     return result;
@@ -23,8 +39,3 @@ commentSchema.methods.recordComment = async function() {
 const commentsModel = db.model('Comments', commentSchema);
 
 module.exports = commentsModel;
-    
-
-
-
-
--- models/reviewsdb.js
+++ models/reviewsdb.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccapdev.eadlr.mongodb.net/ReviewMe?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => { console.log('reviewsdb: OK'); },
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccapdev.eadlr.mongodb.net/ReviewMe?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+            console.log('reviewsdb: OK');
+        },
         err => {
             console.log('reviewsdb: ');
             console.log(err);
@@ -8,14 +13,31 @@ mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@ccap
 var db = mongoose.connection;
 
 const reviewSchema = new mongoose.Schema({
-    reviewID: {type: Number, required: true},
-    postDate: {type: Date, required: true},
-    content: {type: String, required: true},
-    storeRating: {type: Number, required: true},
-    score: {type: Number, required: true}
-}, { collection: "Reviews" });
+    reviewID: {
+        type: Number,
+        required: true
+    },
+    postDate: {
+        type: Date,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    storeRating: {
+        type: Number,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    }
+}, {
+    collection: "Reviews"
+});
 
-reviewSchema.methods.recordReview = async function() {
+reviewSchema.methods.recordReview = async function () {
     var result = reviewsModel.create(this);
     console.log(JSON.stringify(result));
     return result;
